dc2a5d65368f693e94a1351f10f67f15332bb583
yy <yy@DESKTOP-31UG4RA>
2021-06-10 23:05:31 +0300

    stage

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+
+//https://www.calculateme.com/roman-numerals/to-roman
+//http://graecolatini.bsu.by/htm-different/num-converter-roman.htm
+//https://stackoverflow.com/questions/50260758/c-arabic-numbers-to-roman-numerals-vice-versa
+
 int main() {
     
     while (true) {
